23706bb7254003e4c4cb6619445bef58b1cb6359
evencoding <88191233+evencoding@users.noreply.github.com>
2021-08-01 05:17:13 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BaekJoon_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
