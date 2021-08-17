c7a171ffd24474daabb7089498684144aa228b48
evencoding <88191233+evencoding@users.noreply.github.com>
2021-08-18 02:16:10 +0900

    # 2021-08-18

--- Program.cs
+++ Program.cs
@@ -8,39 +8,16 @@ namespace BaekJoon_C_
 {
     class Program
     {
-        static StringBuilder sb;
-        static bool[] isCheaked;
-        static int[] result;
-        static string[] input_num;
-
         static void Main(string[] args)
         {
-            sb = new StringBuilder();
-            input_num = Console.ReadLine().Split();
-            result = new int[int.Parse(input_num[0])];
-            isCheaked = new bool[int.Parse(input_num[1])+1];
-
-            permutation(0);
-        }
-        static void permutation(int num){
-            if(num == int.Parse(input_num[1]))
+            while(true)
             {
-                for (int i = 0; i < num; i++){
-                    sb.Append(result[i] + " ");
-                }
-                Console.WriteLine(sb);
-                sb.Clear();
-                return;
-            }
-            for (int i = 1; i <= int.Parse(input_num[0]); i++)
-            {
-                if(!isCheaked[i])
-                {
-                    isCheaked[i] = true;
-                    result[num] = i;
-                    permutation(num+1);
-                    isCheaked[i] = false;
-                }
+                string input = Console.ReadLine();
+                if(input == null) break;
+                string[] N = input.Split();
+                int a = int.Parse(N[0]);
+                int b = int.Parse(N[1]);
+                Console.WriteLine(a + b);
             }
         }
     }
